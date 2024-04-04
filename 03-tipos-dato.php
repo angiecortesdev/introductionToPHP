735efe4a60248383dbc6bd058093072a6bd77b24
Angie cortes <angiecortes29@gmail.com>
2024-04-04 09:43:16 -0500

    Tipos de datos

---
+++ 03-tipos-dato.php
@@ -0,0 +1,24 @@
+<?php include 'includes/header.php';
+
+//Buleanos - Booleans
+$logueado = false;
+var_dump($logueado);
+
+//Enteros- INTEGERS 
+$numero = 200;
+var_dump($numero);
+
+//Flotantes - Floats
+$float = 20.8;
+var_dump($float);
+
+//Cadena de textos - Strings
+$nombre = 'Angie <br/>';
+var_dump($nombre);
+
+//Arreglos - Arrays
+$array = [];
+var_dump($array);
+
+
+include 'includes/footer.php';
