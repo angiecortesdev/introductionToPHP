fca4423ef8f53b9d71f05e8589c0d6f2c6f4439e
Angie cortes <angiecortes29@gmail.com>
2024-04-04 11:07:24 -0500

    Incremento y decremento

---
+++ 06-inc-dec.php
@@ -0,0 +1,21 @@
+<?php include 'includes/header.php';
+
+//Incremento
+$numero1 = 30;
+$numero1++;
+
+//decremento
+$numero2 = 30;
+$numero2--;
+
+$numero3 = 40;
+$numero3 += 3;
+
+echo $numero1;
+echo '<br />';
+echo $numero2;
+echo '<br />';
+echo $numero3;
+
+
+include 'includes/footer.php';
