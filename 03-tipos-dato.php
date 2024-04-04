4059421c7a66748a34ac3e415fa12160d2c4df94
Angie cortes <angiecortes29@gmail.com>
2024-04-04 09:51:55 -0500

    Operadores

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -3,22 +3,27 @@
 //Buleanos - Booleans
 $logueado = false;
 var_dump($logueado);
+echo '<br />';
 
 //Enteros- INTEGERS 
 $numero = 200;
 var_dump($numero);
+echo '<br />';
 
 //Flotantes - Floats
 $float = 20.8;
 var_dump($float);
+echo '<br />';
 
 //Cadena de textos - Strings
 $nombre = 'Angie <br/>';
 var_dump($nombre);
+echo '<br />';
 
 //Arreglos - Arrays
 $array = [];
 var_dump($array);
+echo '<br />';
 
 
 include 'includes/footer.php';
---
+++ 04-operadores.php
@@ -0,0 +1,23 @@
+<?php include 'includes/header.php';
+
+$numero1 = 20;
+$numero2 = 10;
+
+//Suma
+echo $numero1 + $numero2;
+echo '<br />';
+
+//Restar
+echo $numero1 - $numero2;
+echo '<br />';
+
+//Múltiplicar
+echo $numero1 * $numero2;
+echo '<br />';
+
+//Dividir
+echo $numero1 / $numero2;
+echo '<br />';
+
+
+include 'includes/footer.php';
